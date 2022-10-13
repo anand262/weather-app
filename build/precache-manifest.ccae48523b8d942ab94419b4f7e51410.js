b6d7e8802050240a57d7bdee290788bb4cec2f16
Anand_Shinde <98341248+anand262@users.noreply.github.com>
2022-10-13 11:42:21 +0530

    Add files via upload

---
+++ build/precache-manifest.ccae48523b8d942ab94419b4f7e51410.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d3172f1551eac926aad1",
+    "url": "/static/css/main.d6739276.chunk.css"
+  },
+  {
+    "revision": "d3172f1551eac926aad1",
+    "url": "/static/js/main.d3172f15.chunk.js"
+  },
+  {
+    "revision": "5970b8146f719c92d862",
+    "url": "/static/js/1.5970b814.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ef7e63b7079547b2db6a12d9573a9ba3",
+    "url": "/static/media/cold-bg.ef7e63b7.jpg"
+  },
+  {
+    "revision": "db659feb8c42cafaed602f9b44fe6443",
+    "url": "/index.html"
+  }
+];
